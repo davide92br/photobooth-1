0b38a09bfeb049096db32f99e57c6e1c1a913542
Davide <davide.brugnoni@gmail.com>
2019-02-25 14:32:14 +0100

    add ita language

---
+++ lang/it.js
@@ -0,0 +1,14 @@
+L10N = {
+	"gallery": "Galleria",
+	"startScreen": "<h1>Photobooth<\/h1><h2>Webinterface<\/h2> by Davide Brugnoni Photographer",
+	"takePhoto": "Scatta Foto!",
+	"home": "Home",
+	"qr": "QR Code",
+	"newPhoto": "Nuova Foto",
+	"busy": "Sviluppando",
+	"cheese": "Cheeeeeeeese!",
+	"qrHelp": "Collegati al wifi <strong>photobooth<\/strong>, per scaricare l'immagine sul tuo smartphone.",
+	"error": "Qualcosa è andato storto. Riprova!",
+	"reload": "Ricarica Pagina",
+	"print": "Stampa"
+}

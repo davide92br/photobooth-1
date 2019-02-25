b70b1ae7e254ee0703f8120b2242d81fe3654e81
Davide <davide.brugnoni@gmail.com>
2019-02-25 20:41:19 +0100

    add print cp1200

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 
 $config = array();
 $config['os'] = (DIRECTORY_SEPARATOR == '\\') || (strtolower(substr(PHP_OS, 0, 3)) === 'win') ? 'windows' : 'linux';
-$config['dev'] = true;
+$config['dev'] = false;
 $config['use_print'] = true;
 $config['use_qr'] = true;
 $config['show_fork'] = true;
@@ -10,10 +10,10 @@ $config['show_fork'] = true;
 
 // FOLDERS
 // change the folders to whatever you like
-$config['folders']['images'] = 'images';
-$config['folders']['thumbs'] = 'thumbs';
-$config['folders']['qrcodes'] = 'qrcodes';
-$config['folders']['print'] = 'print';
+$config['folders']['images'] = 'usbhdd/images';
+$config['folders']['thumbs'] = 'usbhdd/thumbs';
+$config['folders']['qrcodes'] = 'usbhdd/qrcodes';
+$config['folders']['print'] = 'usbhdd/print';
 
 // GALLERY
 // should the gallery list the newest pictures first?
@@ -21,29 +21,29 @@ $config['gallery']['newest_first'] = true;
 
 // LANGUAGE
 // possible values: en, de, fr
-$config['language'] = 'de';
+$config['language'] = 'it';
 
 // COMMANDS and MESSAGES
 switch($config['os']) {
-	case 'windows':
-	$config['take_picture']['cmd'] = 'digicamcontrol\CameraControlCmd.exe /capture /filename %s';
-	$config['take_picture']['msg'] = 'Photo transfer done.';
-	$config['print']['cmd'] = 'mspaint /pt "%s"';
-	$config['print']['msg'] = '';
-	break;
-	case 'linux':
-	default:
-	$config['take_picture']['cmd'] = 'sudo gphoto2 --capture-image-and-download --filename=%s images';
-	$config['take_picture']['msg'] = 'New file is in location';
-	$config['print']['cmd'] = 'sudo lp -o landscape -o fit-to-page %s';
-	$config['print']['msg'] = '';
-	break;
+        case 'windows':
+        $config['take_picture']['cmd'] = 'digicamcontrol\CameraControlCmd.exe /capture /filename %s';
+        $config['take_picture']['msg'] = 'Photo transfer done.';
+        $config['print']['cmd'] = 'sudo lp -d PRINTERnameINcups -o landscape -o media=Custom.100x148mm -o fit-to-page %s';
+        $config['print']['msg'] = '';
+        break;
+        case 'linux':
+        default:
+        $config['take_picture']['cmd'] = 'sudo gphoto2 --capture-image-and-download --filename=%s images';
+        $config['take_picture']['msg'] = 'New file is in location';
+        $config['print']['cmd'] = 'sudo lp -o landscape -o fit-to-page %s';
+        $config['print']['msg'] = '';
+        break;
 }
 
 // DON'T MODIFY
 // preparation
 foreach($config['folders'] as $directory) {
-	if(!is_dir($directory)){
-		mkdir($directory, 0777);
-	}
+        if(!is_dir($directory)){
+                mkdir($directory, 0777);
+        }
 }
